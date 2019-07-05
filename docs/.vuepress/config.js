db17e90098d0a209b0412a76e1503236cc7d5800
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2019-07-05 14:19:02 +0430

    chore: add custom style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
-module.exports = {
+module.exports = ctx => ({
   port: 8090,
+  title: 'ViolaCSS',
   description: 'ViolaCSS is a direction-agnostic and mobile-first CSS framework',
-}
+  head: [
+    ['meta', {name: 'theme-color', content: '#7f2f70'}],
+    ['link', {href: 'https://fonts.googleapis.com/css?family=Quicksand:400,500&display=swap', rel: 'stylesheet'}]
+  ]
+})
