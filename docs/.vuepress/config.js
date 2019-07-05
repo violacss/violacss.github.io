5cc259d156b2218cebd9c07f8bd57f1b70c6f3df
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2019-07-05 13:20:31 +0430

    ci: create basic website & config travis

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 8090,
+  description: 'ViolaCSS is a direction-agnostic and mobile-first CSS framework',
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# ViolaCSS Website
+
+ViolaCSS is a direction-agnostic and mobile-first CSS framework
